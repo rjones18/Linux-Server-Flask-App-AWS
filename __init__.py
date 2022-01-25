3fa09ccdba42fd23cbb108e06aca88a70cbdd3c2
Reggie Jones <64709385+rjones18@users.noreply.github.com>
2022-01-25 12:54:06 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "XXXXXXXXX"
 
 @app.route("/")  # this sets the route to this page
 def home():
-	return jsonify({'Message': "This app is run on a Linux Server hosted in AWS!"})
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
